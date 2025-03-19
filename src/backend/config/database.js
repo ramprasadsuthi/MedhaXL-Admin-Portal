9e038489138aaed36798716a61dc88ccbcc8efe4
Bhaskar rao <bhaskarsesetti1.3@gmail.com>
2025-03-19 22:32:37 +0530

    update nrml code

--- src/backend/config/database.js
+++ src/backend/config/database.js
@@ -1,13 +1,17 @@
 const mysql = require('mysql');
 require('dotenv').config();
 
+
+
 const db = mysql.createPool({
+
     connectionLimit: 10,
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASS,
     database: process.env.DB_NAME,
-    port: process.env.DB_PORT
+
+
 });
 
 module.exports = db;
