1191f045517196e4594e3bf32750c6ee3ddaf92b
Bhaskar rao <bhaskarsesetti1.3@gmail.com>
2025-03-20 10:55:45 +0530

    .env file

--- src/backend/config/database.js
+++ src/backend/config/database.js
@@ -9,7 +9,7 @@ const db = mysql.createPool({
     user: process.env.DB_USER,
     password: process.env.DB_PASS,
     database: process.env.DB_NAME,
-    port: process.env.DB_PORT || 3306,
+    port: process.env.DB_PORT || 19882,
 });
 
 
